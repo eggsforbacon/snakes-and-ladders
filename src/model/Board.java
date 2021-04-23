c2c7b56295ea7c171e36fe304e6d91cc9f7b6d2d
1KVueltasAlCampo <angrybirdsgallo@gmail.com>
2021-04-23 01:40:50 -0500

    22.
    Correctly added ladders and snakes (or so I hope)

--- src/model/Board.java
+++ src/model/Board.java
@@ -13,6 +13,7 @@ public class Board {
 	private int size;
 	private SavedNumber first;
 	private String boardString="";
+	private String boxesInformation="";
 
 	
 	public Board(int rows,int columns,int snakes, int ladders, int players) {
@@ -63,23 +64,23 @@ public class Board {
 	
 	private void putLadders(int i,int ladders) {
 		if (i < ladders) {
-			int limit = columns+1;
+			int limit = 1; //antes era columns+1
 			//System.out.println("El limite es "+limit);
 			if(ladders>limit){
 				limit = ladders;
 			}
 			int random = (int) Math.floor(Math.random()*(size-limit-columns)+1);
-			System.out.println(random+" es el numero generado");
+			//System.out.println(random+" es el numero generado");
 			int ladderStart = chooseBoxes(limit-columns,1,random,limit);
 			int aux = numberToHisBase(ladderStart);
-			System.out.println(ladderStart+" es el principio");
-			System.out.println(aux+" es el numero base");
-			System.out.println("Empieza desde: "+(ladderStart+((columns+1)-aux)));
+			//System.out.println(ladderStart+" es el principio");
+			//System.out.println(aux+" es el numero base");
+			//System.out.println("Empieza desde: "+(ladderStart+((columns+1)-aux)));
 			int random2 = (int) Math.floor(Math.random()*(size-(ladderStart+((columns+1)-aux)))+(ladderStart+((columns+1)-aux)));
-			System.out.println("Originalmente era "+random2);
+			//System.out.println("Originalmente era "+random2);
 			int ladderEnd = chooseBoxes(ladderStart+((columns+1)-aux),ladderStart+((columns+1)-aux),random2,1);
-			System.out.println(ladderStart);
-			System.out.println(ladderEnd);
+			//System.out.println(ladderStart);
+			//System.out.println(ladderEnd);
 			getANode(ladderStart, upper(matrix)).setStart(true); 
 			getANode(ladderStart, upper(matrix)).setLadder(getANode(ladderEnd, upper(matrix)));
 			getANode(ladderEnd, upper(matrix)).setEnd(true);
@@ -103,18 +104,41 @@ public class Board {
 
 	private void putSnakes(int i,int snakes){
 		if(i<snakes){
-			int limit = rows+1;
-			//System.out.println("El limite es "+limit);
+			int limit = columns+1;
+
 			if(snakes>limit){
 				limit = snakes;
-				System.out.println("El limite son las serpientes");
+				//System.out.println("El limite son las serpientes");
 			}
 			int random = (int) Math.floor(Math.random()*(size-limit-2)+limit);
-			//System.out.println(random);
+			//System.out.println("El limite es "+limit);
+			//System.out.println(random+" El primer random");
+			int snakeHead = chooseBoxesForSnakes(limit-2,limit,random);
+			//System.out.println(snakeHead+" nuevo valor");
+			int aux = numberToHisBase(snakeHead);
+			int random2 = (int) Math.floor(Math.random()*(snakeHead-aux)+1);
+			//System.out.println("El segundo random es "+random2);
+			int snakeTail = chooseBoxesForSnakes(snakeHead-aux,1,random2);
+			//System.out.println(snakeTail+" nuevo valor");
+			getANode(snakeHead, upper(matrix)).setHead(true);
+			getANode(snakeHead, upper(matrix)).setSnake(getANode(snakeTail, upper(matrix)));
+			getANode(snakeTail, upper(matrix)).setTail(true);
 			putSnakes(i+1,snakes);
 		}
 	}
-	
+	public int chooseBoxesForSnakes(int a,int b,int number){
+		if(!cycleThroughSavedNumbers(number, first)) {
+			addToSavedNumbers(number);
+			return number;
+		}
+		else{
+			//System.out.println(number+" esta ocupado");
+			if(number-1<b){
+				number = (int)Math.floor(Math.random()*(size-a)+b);
+			}
+			return chooseBoxesForSnakes(a,b,number-1);
+		}
+	}
 	public int chooseBoxes(int a,int b,int number,int limit) {
 			if(!cycleThroughSavedNumbers(number, first)) {
 				addToSavedNumbers(number);
@@ -123,7 +147,7 @@ public class Board {
 			else {
 				if(number >= size-limit) {
 					number = (int)Math.floor(Math.random()*(size-a)+b);
-					System.out.println("se propone "+number);
+					//System.out.println("se propone "+number);
 				}
 				return chooseBoxes(a,b,number+1,limit);
 			}	
@@ -142,6 +166,7 @@ public class Board {
 			return false;
 		}
 		if(number == toFind.getData()) {
+			//System.out.println(" Si lo encontro");
 			return true;
 		}
 		else{
@@ -153,9 +178,23 @@ public class Board {
 		Node last = upper(matrix);
 		toDown(last);
 		return boardString;
-		
-		
-		
+	}
+
+	public String getBoxesInformation(){
+		Node last = upper(matrix);
+		seeInformation(last);
+		return boxesInformation;
+	}
+
+	public boolean seeInformation(Node down) {
+		if(down.getDown()==null) {
+			anotherToTheRight(down);
+			return true;
+		}
+		else {
+			anotherToTheRight(down);
+			return seeInformation(down.getDown());
+		}
 	}
 	
 	public Node getANode(int number,Node node) {
@@ -235,6 +274,27 @@ public class Board {
 				boardString += " ] ";
 			}
 			return toTheRight(right.getNext());
+		}
+	}
+
+	public Node anotherToTheRight(Node right) {
+		if(right.getNext()==null) {
+			if(right.isStart()) {
+				boxesInformation += "LADDER: "+right.getPosition()+" -> "+right.getLadder().getPosition()+"\n";
+			}
+			else if(right.isHead()){
+				boxesInformation += "SNAKE: "+right.getPosition()+" -> "+right.getSnake().getPosition()+"\n";
+			}
+			return right;
+		}
+		else {
+			if(right.isStart()) {
+				boxesInformation += "LADDER: "+right.getPosition()+" -> "+right.getLadder().getPosition()+"\n";
+			}
+			else if(right.isHead()){
+				boxesInformation += "SNAKE: "+right.getPosition()+" -> "+right.getSnake().getPosition()+"\n";
+			}
+			return anotherToTheRight(right.getNext());
 		}
 	}
 	
--- src/ui/Menu.java
+++ src/ui/Menu.java
@@ -6,7 +6,8 @@ public class Menu {
 	
 	private Board board;
     public void startProgram() {
-    	board = new Board(6,2,1,3,4);
+    	board = new Board(5,4,2,3,4);
     	System.out.println(board.getMatrix());
+    	System.out.println(board.getBoxesInformation());
     }
 }
